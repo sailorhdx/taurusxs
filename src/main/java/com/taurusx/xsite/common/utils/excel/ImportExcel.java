2620ee634936c4e00a94f89375b31092298c4b9b
sailorhdx <sailorhdx@hotmail.com>
2018-02-26 17:25:13 +0800

    导入excel消除科学记数法

--- src/main/java/com/taurusx/xsite/common/utils/excel/ImportExcel.java
+++ src/main/java/com/taurusx/xsite/common/utils/excel/ImportExcel.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -215,7 +216,9 @@ public class ImportExcel {
 			Cell cell = row.getCell(column);
 			if (cell != null) {
 				if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-					val = cell.getNumericCellValue();
+					// val = cell.getNumericCellValue();
+					DecimalFormat df = new DecimalFormat("0");
+					val = df.format(cell.getNumericCellValue());
 				} else if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
 					val = cell.getStringCellValue();
 				} else if (cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
